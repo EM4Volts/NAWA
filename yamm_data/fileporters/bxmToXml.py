6e30e2057b4b178301e99e70ec876230c38c4708
EM4Volts <em4volts@gmail.com>
2022-10-21 22:38:16 +0200

    upgraded to 1.3.1b

---
+++ yamm_data/fileporters/bxmToXml.py
@@ -0,0 +1,29 @@
+import os
+import yamm_data.fileporters.bxm as bxm
+import sys
+import xml.dom.minidom
+import xml.etree.ElementTree as ET
+
+
+def main(bxmFils):
+	bxmFiles = [bxmFils]
+	if len(bxmFiles) == 1 and len(sys.argv) >= 3:
+		xmlFiles = sys.argv[2:]
+	else:
+		xmlFiles = [os.path.splitext(f)[0] + '.xml' for f in bxmFiles]
+
+	for i, bxmFile in enumerate(bxmFiles):
+		xmlResult = bxm.bxmToXml(bxmFile)
+
+		try:
+			xmlStr = ET.tostring(xmlResult.toXml())
+			if type(xmlStr) == bytes:
+				xmlStr = xmlStr.decode("utf-8")
+			dom = xml.dom.minidom.parseString(xmlStr)
+			xmlStr = dom.toprettyxml(indent="\t")
+		except:
+			print("Warning: using fallback string representation")
+			xmlStr = str(xmlResult)
+
+		with open(xmlFiles[i], "w", encoding="utf-8") as f:
+			f.write(xmlStr)
